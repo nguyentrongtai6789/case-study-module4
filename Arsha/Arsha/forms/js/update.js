3a0944b5a4ef74f14c3837f3d8bd4359deb51fc6
Trung Do CodeGym <trung96ecvn@gmail.com>
2023-10-04 10:29:36 +0700

    sửa phương thức update blog

--- Arsha/Arsha/forms/js/update.js
+++ Arsha/Arsha/forms/js/update.js
@@ -2,17 +2,34 @@ function update(id_blog) {
     localStorage.setItem("id_blog", id_blog)
     window.location.href = "../html/index.html";
 }
+function showCategory() {
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/api/",
+        success:function (data){
+            let content = "";
+            if (data.length !== null){
+                for (let i = 0; i < data.length; i++) {
+                    content += "<option value=\"" + data[i].id + "\">" + data[i].name + "</option>";
+                }
+                document.getElementById("category").value = content;
+            }
+        }
+    })
+}
 function updateBlog() {
     let id_blog = +localStorage.getItem("id_blog");
+    showCategory();
     $.ajax({
         type: "GET",
         url: "http://localhost:8080/api/blog/update/" + id_blog,
         success: function (data){
-            document.getElementById("title").values = data.title;
-            document.getElementById("content").values = data.content;
-            document.getElementById("date").values = data.date;
-            document.getElementById("account").values = data.account.id;
-            document.getElementById("category").values = data.category.id;
+            document.getElementById("title").value = data.title;
+            document.getElementById("content").value = data.content;
+            document.getElementById("date").value = data.date;
+            document.getElementById("account").value = data.account.name;
+            document.getElementById("category").value = data.category.id;
+            localStorage.setItem("id_account_update",data.account.id)
         }
     })
 }
@@ -25,7 +42,7 @@ function updateNewBlog() {
     let account = null;
     let category = null;
     let file = document.getElementById("file")[0].files[0];
-    let id_account = document.getElementById("account").value;
+    let id_account = +localStorage.getItem("id_account_update");
     let id_category = document.getElementById("category").value;
     let blog = {
         id_blog: id_blog,
